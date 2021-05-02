f7997ed4285a5298c3dc29d66b2b1cb4cdfa23bc
Simon Boisset <sboisset@ambuliz.com>
2021-05-02 02:19:33 +0200

    Fix types

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,29 +1,29 @@
 import { useCallback, useMemo, useState } from 'react';
-import { Asserts, SchemaOf, ValidationError } from 'yup';
+import { SchemaOf, ValidationError } from 'yup';
 import { InputProps } from '../types/InputProps';
 import { SchemaError } from '../types/SchemaError';
 import { fieldsFactory } from '../utils/fieldsFactory';
 
-export type FieldsType<T> = { [key in keyof Asserts<SchemaOf<T>>]: InputProps<Asserts<SchemaOf<T>>[key]> };
+export type FieldsType<T> = { [key in keyof T]: InputProps<T[key]> };
 
 export type UseFormType<T> = {
   submit: (e: React.FormEvent<HTMLFormElement>) => void;
 } & FieldsType<T>;
 
-export const useForm = <T>(schema: SchemaOf<T>, onSubmit?: (value: Asserts<typeof schema>) => void): UseFormType<T> => {
-  const defaultValue = useMemo(() => schema.getDefault() as Asserts<typeof schema>, []);
-  const [formValue, setFormValue] = useState<Asserts<typeof schema>>(defaultValue);
-  const [formErrors, setFormErrors] = useState<SchemaError<Asserts<typeof schema>>>({});
+export const useForm = <T>(schema: SchemaOf<T>, onSubmit?: (value: T) => void): UseFormType<T> => {
+  const defaultValue = useMemo(() => schema.getDefault() as T, []);
+  const [formValue, setFormValue] = useState<T>(defaultValue);
+  const [formErrors, setFormErrors] = useState<SchemaError<T>>({});
 
   const validate = useCallback(
-    (value: Asserts<typeof schema>) => {
+    (value: T) => {
       let nextError: ValidationError[] | null = null;
       try {
         schema.validateSync(value, { abortEarly: false, stripUnknown: true });
       } catch (errors) {
         nextError = errors.inner as ValidationError[];
       }
-      let error: SchemaError<Asserts<typeof schema>> = {};
+      let error: SchemaError<T> = {};
       if (nextError) {
         for (const err of nextError) {
           const path = err.path?.split('.');
