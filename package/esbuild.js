f0e6386e3b42ad960b401a49b63d92b2333c7233
Simon Boisset <simon.boisset@gmail.com>
2021-10-16 14:17:08 +0200

    Fix esbuild module (#25)
    * v1.1.2
    
    * v1.1.3
    
    * v1.1.4
    
    * v1.1.5
    
    * v1.1.6
    
    * v1.1.7
    
    * v1.1.8
    
    * v1.1.9
    
    * v1.1.10
    
    * Fix esbuild module

--- package/esbuild.js
+++ package/esbuild.js
@@ -1,15 +1,16 @@
 const esbuild = require('esbuild');
-
+const { nodeExternalsPlugin } = require('esbuild-node-externals');
 esbuild
   .build({
     entryPoints: ['./src/index.ts'],
-    outdir: 'dist',
+    outfile: 'dist/index.js',
     bundle: true,
     sourcemap: true,
     minify: true,
     treeShaking: true,
-    splitting: true,
-    format: 'esm',
-    target: ['esnext'],
-  })
+    platform: 'node',
+    format: 'cjs',
+    target: 'node14',
+    plugins: [nodeExternalsPlugin()],
+  }) //@ts-ignore
   .catch(() => process.exit(1));
